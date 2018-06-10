25ccfab8fa45bea7afc245445e9763dcd077812a
周立 <li.zhou@huilianyi.com>
2018-06-10 18:21:16 +0800

    a

---
+++ src/drag/canvas-event/js/shape/DisplayObject.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ZhangJikai on 2016/12/3.
+ */
+
+(function () {
+    cce.DisplayObject = function () {
+        cce.EventTarget.call(this);
+        //console.log(2222)
+        this.canvas = null;
+        this.context = null;
+
+
+    };
+
+    cce.DisplayObject.prototype = Object.create(cce.EventTarget.prototype);
+    cce.DisplayObject.prototype.constructor = cce.DisplayObject;
+
+
+    cce.DisplayObject.prototype.compareTo = function (target) {
+        return null;
+    };
+
+    cce.DisplayObject.prototype.comparePointX = function (point) {
+        return null;
+    };
+
+    cce.DisplayObject.prototype.hasPoint = function (point) {
+        return false;
+    };
+
+}());
